7b904cc6af700634293bc7cd6d05c02b495c76fe
carlybrail <69254570+carlybrail@users.noreply.github.com>
2020-08-05 21:55:41 -0400

    Add files via upload
    my mini project from Xcode

---
+++ miniproject.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aboutme
+//
+//  Created by Carly Brail on 8/5/20.
+//  Copyright © 2020 Carly Brail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
